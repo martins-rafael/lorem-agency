b45cf0f167e03170c8b6d760d42960373e1ceebe
Rafael Martins <rafaeldcmartins@gmail.com>
2020-06-26 21:10:15 -0300

    add parallax effect and a cool progress bar

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+
+    //Progress bar
+    let containerA = document.getElementById('circleA');
+    let containerB = document.getElementById('circleB');
+    let containerC = document.getElementById('circleC');
+    let containerD = document.getElementById('circleD');
+
+    let circleA = new ProgressBar.Circle(containerA, {
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 1400,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 60);
+
+            circle.setText(value);
+        }
+    });
+
+    let circleB = new ProgressBar.Circle(containerB, {
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 1600,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 254);
+
+            circle.setText(value);
+        }
+    });
+
+    let circleC = new ProgressBar.Circle(containerC, {
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 2000,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 32);
+
+            circle.setText(value);
+        }
+    });
+
+    let circleD = new ProgressBar.Circle(containerD, {
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 2200,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 5243);
+
+            circle.setText(value);
+        }
+    });
+
+    let dataAreaOffset = $('#data-area').offset();
+    let stop = 0;
+
+    $(window).scroll(function (e) {
+        let scroll = $(window).scrollTop();
+
+        if (scroll > (dataAreaOffset.top - 500) && stop == 0) {
+            circleA.animate(1.0);
+            circleB.animate(1.0);
+            circleC.animate(1.0);
+            circleD.animate(1.0);
+
+            stop = 1;
+        }
+    })
+});
+
+// Parallax
+setTimeout(function () {
+    $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' })
+}, 250)
